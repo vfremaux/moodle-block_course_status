ae163ee03fe1d979cd3cdc0f0eebcd190921bacb
valery <valery.fremaux@gmail.com>
2016-09-28 11:38:27 +0200

    automated sync from SVN

---
+++ styles.php
@@ -0,0 +1,7 @@
+span.coursestatus{
+	font-size:1.6em;
+	font-weight:bolder;
+	font-family:sans-serif;
+	backgound-color:#A0A0A0;
+	
+}
--- version.php
+++ version.php
@@ -17,20 +17,20 @@
 /**
  * Version details.
  *
- * @package     block_course_course_status
+ * @package     course_course_status
  * @category    blocks
- * @author      Valery Fremaux <valery.fremaux@gmail.com>
- * @copyright   2010 onwards Valery Fremaux (http://www.mylearningfactory.com)
+ * @author      Valery Fremaux (valery.fremaux@gmeil.com)
+ * @copyright   2009 onwards Valery Fremaux (valery.fremaux@gmeil.com)
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015062400;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2014110400;        // Requires this Moodle version
-$plugin->release   = "2.8.0 Build(2015062400)";
+$plugin->version   = 2014091000;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2015050500;        // Requires this Moodle version
+$plugin->release   = "2.9.0 Build(2014091000)";
 $plugin->maturity   = MATURITY_RC;
 $plugin->component = 'block_course_status'; // Full name of the plugin (used for diagnostics)
 
 // Non moodle attributes.
-$plugin->codeincrement = '2.8.0000';
+$plugin->codeincrement = '2.9.0000';
